22007263f547318b94688b074a64d4a2f1262c07
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-03-06 12:51:47 -0300

    First working version

--- OptionalOutlets/OptionalOutlets.m
+++ OptionalOutlets/OptionalOutlets.m
@@ -13,6 +13,12 @@
 @property (nonatomic, strong, readwrite) NSBundle *bundle;
 @end
 
+@interface DVTTextStorage : NSTextStorage
+
+- (void)replaceCharactersInRange:(struct _NSRange)arg1 withString:(nonnull NSString *)arg2 withUndoManager:(nullable id)arg3;
+
+@end
+
 @implementation OptionalOutlets
 
 + (instancetype)sharedPlugin
@@ -20,9 +26,9 @@
     return sharedPlugin;
 }
 
-- (id)initWithBundle:(NSBundle *)plugin
-{
-    if (self = [super init]) {
+- (instancetype)initWithBundle:(NSBundle *)plugin {
+    self = [super init];
+    if (self) {
         // reference to plugin's bundle, for resource access
         self.bundle = plugin;
         [[NSNotificationCenter defaultCenter] addObserver:self
@@ -33,33 +39,72 @@
     return self;
 }
 
-- (void)didApplicationFinishLaunchingNotification:(NSNotification*)noti
-{
+- (void)didApplicationFinishLaunchingNotification:(NSNotification*)notification {
     //removeObserver
     [[NSNotificationCenter defaultCenter] removeObserver:self name:NSApplicationDidFinishLaunchingNotification object:nil];
     
-    // Create menu items, initialize UI, etc.
-    // Sample Menu Item:
-    NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
-    if (menuItem) {
-        [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
-        //[actionMenuItem setKeyEquivalentModifierMask:NSAlphaShiftKeyMask | NSControlKeyMask];
-        [actionMenuItem setTarget:self];
-        [[menuItem submenu] addItem:actionMenuItem];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(editorDocumentDidChange:) name:@"IDEEditorDocumentDidChangeNotification" object:nil];
+}
+
+
+- (void)editorDocumentDidChange:(NSNotification *)notification {
+    NSArray *array = notification.userInfo[@"IDEEditorDocumentChangeLocationsKey"];
+    id firstChange = array.firstObject;
+    
+    NSURL *url = [firstChange valueForKey:@"documentURL"];
+    if (![url isKindOfClass:[NSNull class]]) {
+        NSRange range = [[firstChange valueForKey:@"characterRange"] rangeValue];
+        
+        // Check if it's a valid range
+        if (range.length == 0 || range.location == NSNotFound) {
+            return;
+        }
+        
+        // Check if it's an IDESourceCodeDocument
+        if (![notification.object respondsToSelector:@selector(textStorage)]) {
+            return;
+        }
+        
+        id textStorage = [notification.object textStorage];
+        NSAttributedString *attributed;
+        
+        NSRange newRange = NSMakeRange(range.location, range.length + 3);
+        //Fixes a bug where the last three characters would not be present
+        
+        @try {
+            attributed = [textStorage attributedSubstringFromRange:newRange];
+        }
+        @catch (NSException *exception) {
+            return;
+        }
+        
+        NSString *substring = attributed.string;
+        
+        NSString *updatedSubstring = [self fixedStringForString: substring];
+        if (![updatedSubstring isEqualToString:substring]) {
+            [textStorage beginEditing];
+            [textStorage replaceCharactersInRange:newRange withString:updatedSubstring withUndoManager:nil];
+            [textStorage endEditing];
+        }
     }
 }
 
-// Sample Action, for menu item:
-- (void)doMenuAction
-{
-    NSAlert *alert = [[NSAlert alloc] init];
-    [alert setMessageText:@"Hello, World"];
-    [alert runModal];
+- (nonnull NSString *)fixedStringForString:(nonnull NSString *)string {
+    
+    NSString *regexPattern = @"^(\\s*@IBOutlet[\\w:\\s]*)!";
+    NSString *replaceTemplate = @"$1?";
+    
+    NSMutableString *fixedString = [string mutableCopy];
+    NSRange fullRange = (NSRange){0, fixedString.length};
+    
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:regexPattern options:0 error:nil];
+    
+    NSUInteger replaces = [regex replaceMatchesInString:fixedString options:0 range:fullRange withTemplate:replaceTemplate];
+    
+    return replaces == 1 ? fixedString : string;
 }
 
-- (void)dealloc
-{
+- (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
